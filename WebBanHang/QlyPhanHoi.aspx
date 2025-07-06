72a41e3e70f122a75bc15a73b3e80352f0d1cf9c
QuocNhat88 <nnhat425@gmail.com>
2025-07-06 23:31:23 +0700

    đã thêm được quản lý phản hồi nhưng bấm trả lời chưa hiện gì hết

--- WebBanHang/QlyPhanHoi.aspx
+++ WebBanHang/QlyPhanHoi.aspx
@@ -1,6 +1,76 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="QlyPhanHoi.aspx.cs" Inherits="WebBanHang.QlyPhanHoi" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+    <style>
+        h2 {
+            text-align: center;
+            margin-bottom: 30px;
+            color: #333;
+        }
+
+        .phanhoi-table {
+            width: 100%;
+            border-collapse: collapse;
+            font-family: 'Segoe UI', sans-serif;
+        }
+
+        .phanhoi-table th, .phanhoi-table td {
+            padding: 12px 15px;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+
+        .phanhoi-table th {
+            background-color: #f4f4f4;
+            color: #333;
+        }
+
+        .phanhoi-table tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        .phanhoi-table tr:hover {
+            background-color: #f1f1f1;
+        }
+
+        .btn-phanhoi {
+            background-color: #28a745;
+            color: white;
+            padding: 6px 12px;
+            border: none;
+            border-radius: 4px;
+            font-size: 14px;
+            cursor: pointer;
+            transition: background-color 0.2s ease;
+        }
+
+        .btn-phanhoi:hover {
+            background-color: #218838;
+        }
+
+        .grid-wrapper {
+            overflow-x: auto;
+        }
+    </style>
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-    <h1>ahihi</h1>
+    <h2>Quản lý phản hồi khách hàng</h2>
+    <div class="grid-wrapper">
+        <asp:GridView ID="gvPhanHoi" runat="server" AutoGenerateColumns="False" CssClass="phanhoi-table" OnRowCommand="gvPhanHoi_RowCommand">
+            <Columns>
+                <asp:BoundField DataField="LienHeID" HeaderText="ID" />
+                <asp:BoundField DataField="HoTen" HeaderText="Họ tên" />
+                <asp:BoundField DataField="Email" HeaderText="Email" />
+                <asp:BoundField DataField="TieuDe" HeaderText="Tiêu đề" />
+                <asp:BoundField DataField="NoiDung" HeaderText="Nội dung" />
+                <asp:BoundField DataField="NgayGui" HeaderText="Ngày gửi" DataFormatString="{0:dd/MM/yyyy HH:mm}" />
+                <asp:TemplateField HeaderText="Phản hồi">
+                    <ItemTemplate>
+                        <asp:Button ID="btnTraLoi" runat="server" Text="Trả lời" CommandName="TraLoi" CommandArgument='<%# Eval("LienHeID") %>' CssClass="btn-phanhoi" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </div>
 </asp:Content>
--- WebBanHang/QlyPhanHoi.aspx.cs
+++ WebBanHang/QlyPhanHoi.aspx.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
 using System.Web.UI.WebControls;
 
 namespace WebBanHang
@@ -11,7 +10,84 @@ namespace WebBanHang
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                LoadPhanHoi();
+            }
+        }
 
+        private void LoadPhanHoi()
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["BanHangConnectionString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "SELECT * FROM LienHe WHERE DaPhanHoi = 0 ORDER BY NgayGui DESC";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvPhanHoi.DataSource = dt;
+                gvPhanHoi.DataBind();
+            }
+        }
+
+        protected void gvPhanHoi_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "TraLoi")
+            {
+                int lienHeId = Convert.ToInt32(e.CommandArgument);
+                TraLoiPhanHoi(lienHeId);
+                LoadPhanHoi();
+            }
+        }
+
+        private void TraLoiPhanHoi(int lienHeId)
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["BanHangConnectionString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string query = "SELECT * FROM LienHe WHERE LienHeID = @LienHeID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@LienHeID", lienHeId);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    string email = reader["Email"].ToString();
+                    string hoten = reader["HoTen"].ToString();
+                    string tieuDe = reader["TieuDe"].ToString();
+                    string noiDung = reader["NoiDung"].ToString();
+
+                    reader.Close();
+
+                    // Gửi email trả lời
+                    try
+                    {
+                        MailMessage mail = new MailMessage();
+                        mail.To.Add(email);
+                        mail.From = new MailAddress("youremail@gmail.com", "Website Bán Hàng");
+                        mail.Subject = "Phản hồi liên hệ từ Website Bán Hàng";
+                        mail.Body = $"Xin chào {hoten},\n\nChúng tôi đã nhận được phản hồi của bạn với tiêu đề \"{tieuDe}\".\n\nNội dung bạn gửi: {noiDung}\n\nChúng tôi sẽ liên hệ lại sớm nhất.\n\nTrân trọng,\nWebBanHang Team";
+                        mail.IsBodyHtml = false;
+
+                        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                        smtp.Credentials = new System.Net.NetworkCredential("youremail@gmail.com", "your_app_password");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+
+                        // Cập nhật cờ đã phản hồi
+                        SqlCommand updateCmd = new SqlCommand("UPDATE LienHe SET DaPhanHoi = 1 WHERE LienHeID = @LienHeID", conn);
+                        updateCmd.Parameters.AddWithValue("@LienHeID", lienHeId);
+                        updateCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Có thể ghi log lỗi gửi email
+                        throw new Exception("Lỗi gửi email: " + ex.Message);
+                    }
+                }
+            }
         }
     }
 }
